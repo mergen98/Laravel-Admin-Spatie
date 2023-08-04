6f8e068a2e2d7c40303d44346951703cd4084550
MERGENNURYYEV <mergen20.07.1998@gmail.com>
2023-08-04 21:09:46 +0400

    added permissions and roles

---
+++ app/Http/Controllers/Admin/PermissionController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class PermissionController extends Controller
+{
+    public function index()
+    {
+        $permissions = Permission::all();
+        return view('admin.permissions.index',compact('permissions'));
+    }
+    
+    public function create(){
+        return view('admin.permissions.create');
+    }
+    
+    public function store(Request $request){
+        $validated = $request->validate([
+            'name' => ['required','min:3']
+        ]);
+        Permission::create($validated);
+        return to_route('admin.permissions.index')->with('message','Role Created successfully');
+    }
+    
+    public function edit(Permission $permission){
+        return view('admin.permissions.edit',compact('permission'));
+    }
+    
+    public function update(Request $request, Permission $permission){
+        $validated = $request->validate([
+            'name' => ['required','min:3']
+        ]);
+        $permission->update($validated);
+        return to_route('admin.permissions.index')->with('message','Permission Updated successfully');
+    }
+    
+    public function destroy(Permission $permission){
+        $permission->delete();
+        return back()->with('message','Permission Deleted successfully');
+    }
+}
